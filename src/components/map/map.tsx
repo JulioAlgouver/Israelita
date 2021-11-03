7521ab084eed805adc6ce537f202faab8187e4c8
JulioAlgouver <julioalgouver@gmail.com>
2021-11-03 17:52:38 -0300

    Initialize project using Create React App

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -3,11 +3,15 @@ import React from "react";
 //CSS
 import './map.css'
 
+function changeMap(){
+  
+}
+
 const Mapa: React.FC = () => {
   return(
     <>
     <div id='main_map'>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1272.1023054408467!2d-49.37707924776662!3d-25.603299055497565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dd01f7e76c6a9b%3A0xf43d659976a20db!2sR.%20Francisco%20Raksa%20J%C3%BAnior%2C%20167%20-%20Costeira%2C%20Arauc%C3%A1ria%20-%20PR%2C%2083709-170!5e0!3m2!1spt-BR!2sbr!4v1635969587317!5m2!1spt-BR!2sbr"/>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115162.94992544627!2d-49.44205219543946!3d-25.57691670774865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dd02f54dadbe6b%3A0xb140cbe0e164b0f4!2zQXJhdWPDoXJpYSwgUFI!5e0!3m2!1spt-BR!2sbr!4v1635971357884!5m2!1spt-BR!2sbr"/>
     </div>
     </>
   )
