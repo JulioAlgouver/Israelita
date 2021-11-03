76e8dfed58017bc290e52dd7bc8dad2b10d738a8
JulioAlgouver <julioalgouver@gmail.com>
2021-11-03 09:31:30 -0300

    Initialize project using Create React App

---
+++ src/components/menu/menu.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+//IMAGES
+import logo from '../../images/logoMinisterio.png';
+
+
+
+import './menu.css';
+
+const Menu: React.FC = () => {
+  return(
+    <div id="main_landing">
+            <div className="header">
+                <div className="logo">
+                    <h6>
+                    <img src={logo}/>
+                    IGREJA EVANGÉLICA ASSEMBLÉIA DE DEUS<br/>
+                    MINISTÉRIO ISRAELITA
+                    </h6>
+                </div>
+                <div className="menu">
+                        <button>
+                            <Link to="/home" className="linkHome">Home</Link>
+                        </button>
+                        
+                        <button>
+                            <Link to="/agenda" className="linkHome">Agenda</Link>
+                        </button>
+
+                        <button>    
+                            <Link to="/cultos" className="linkHome">Nossos cultos</Link>
+                        </button>
+
+                        <button>    
+                            <Link to="/radio" className="linkHome">Web-Rádio</Link>
+                        </button>
+
+                        <button>    
+                            <Link to="/testemunhos" className="linkHome">Testemunhos</Link>
+                        </button>
+
+                        <button>
+                            <Link to="/oferta" className="linkHome">Oferta</Link>
+                        </button>
+
+                        <button>
+                            <Link to="/oferta" className="linkHome">Contato</Link>
+                        </button>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Menu;
