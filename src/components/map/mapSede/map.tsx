33189648239b6bd78f9bf26645c1b2f85d7153de
JulioAlgouver <julioalgouver@gmail.com>
2021-11-05 14:01:03 -0300

    Initialize project using Create React App

--- src/components/map/mapSede/map.tsx
+++ src/components/map/mapSede/map.tsx
@@ -5,7 +5,7 @@ import Menu from "../../../components/menu/menu";
 //CSS
 import './map.css';
 
-var map='<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3598.0467074473054!2d-49.378631685330134!3d-25.603364146032117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dd01f7e66f6143%3A0x8396236136cf4b!2sR.%20Francisco%20Raksa%20J%C3%BAnior%2C%20155%20-%20Costeira%2C%20Arauc%C3%A1ria%20-%20PR%2C%2083709-170!5e0!3m2!1spt-BR!2sbr!4v1636118084249!5m2!1spt-BR!2sbr';
+var map='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1512.7933898881051!2d-49.37737016523881!3d-25.60327714253491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dd01f7e66f6143%3A0x8396236136cf4b!2sR.%20Francisco%20Raksa%20J%C3%BAnior%2C%20155%20-%20Costeira%2C%20Arauc%C3%A1ria%20-%20PR%2C%2083709-170!5e0!3m2!1spt-BR!2sbr!4v1636129145117!5m2!1spt-BR!2sbr';
 
 const MapSede: React.FC = () => {
   return(
@@ -20,7 +20,7 @@ const MapSede: React.FC = () => {
 
           <button className='btn_sede' type='button' name='btn_sede'>
           <Link to ='/contato/sede' className='link'>
-          <strong>
+            <strong>
               Sede - Costeira
             </strong>
             </Link>
