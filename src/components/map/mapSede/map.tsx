66715e2dfae6b171249b67536a85315c141795b6
JulioAlgouver <julioalgouver@gmail.com>
2021-11-05 15:12:48 -0300

    Initialize project using Create React App

--- src/components/map/mapSede/map.tsx
+++ src/components/map/mapSede/map.tsx
@@ -21,16 +21,30 @@ const MapSede: React.FC = () => {
           <button className='btn_sede' type='button' name='btn_sede'>
           <Link to ='/contato/sede' className='link'>
             <strong>
-              Sede - Costeira
+              Sede - Costeira<br/>
             </strong>
+            <div className='endereco'>
+                <span>
+                  Rua Francisco Raksa Junior, 167<br/>
+                  Costeira - Araucária<br/>
+                  CEP: 83709-170<br/>
+                </span>
+              </div>
             </Link>
           </button>
 
           <button className='btn_vilaSandra' type='button' name='btn_vilaSandra'>
           <Link to ='/contato/vilaSandra' className='link'>
             <strong>
-              Vila Sandra
+              Vila Sandra<br/>
             </strong>
+            <div className='endereco'>
+              <span>
+                Rua Cidade de Pomerode, 491<br/>
+                CIC - Curitiba<br/>
+                CEP: 81230-130<br/>
+              </span>
+            </div>
           </Link>
           </button>
         </div>
