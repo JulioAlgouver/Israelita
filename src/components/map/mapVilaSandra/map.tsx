33626c3cdf1a191b1f890589856b2cc05b446507
JulioAlgouver <julioalgouver@gmail.com>
2021-11-08 16:55:18 -0300

    Initialize project using Create React App

--- src/components/map/mapVilaSandra/map.tsx
+++ src/components/map/mapVilaSandra/map.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Menu from "../../../components/menu/menu";
 
+//IMAGES
+import imgIgreja from '../../../images/vilaSandra_fachada.jpg';
+
 //CSS
 import './map.css';
 
 var map='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1514.5381438357324!2d-49.34317660865014!3d-25.465027302793064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce3d69b29c8c5%3A0xf39660219cd0c23e!2sIgreja%20Evang%C3%A9lica%20Assembl%C3%A9ia%20de%20Deus%20Vila%20Sandra%20(CIC)%20em%20Curitiba%20-%20Minist%C3%A9rio%20Israelita!5e0!3m2!1spt-BR!2sbr!4v1636118509821!5m2!1spt-BR!2sbr';
+
 
 const MapVilaSandra: React.FC = () => {
   return(
@@ -49,7 +53,15 @@ const MapVilaSandra: React.FC = () => {
         </button>
 
         <div className='imagem_igreja'>
+          <img src={imgIgreja} width='250px'/>
+        </div>
 
+        <div className='btn_facebook'>
+          <button>
+            <a href='https://www.facebook.com/assembleiadedeusvilasandraemcuritiba' target='_blank' className='linkButton'>
+              Facebook
+            </a>
+          </button>
         </div>
       </div>
     </div>
