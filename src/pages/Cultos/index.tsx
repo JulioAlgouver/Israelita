8fd58da03013b1dd517452e259b4b13cc9dfb586
JulioAlgouver <julioalgouver@gmail.com>
2021-11-14 16:52:50 -0300

    Initialize project using Create React App

--- src/pages/Cultos/index.tsx
+++ src/pages/Cultos/index.tsx
@@ -9,7 +9,69 @@ const Cultos: React.FC = () => {
     <>
     <Menu/>
     <div id='content_cultos'>
+    <strong>Cultos</strong>
+    <div className='cultos_geral'>
+      <div className='cultos_sede'>
+        <span>Sede</span>
+          <div className='domingo'>
+            <strong>Domingo</strong>
+            <h6>
+              EBD (Escola Bíblica Dominical) - 9h30min 
+              <p/><br/>
 
+              1º Domingo - Culto de Primícias  - 19h<p/>
+              2º Domingo - Culto de Ceia do Senhor - 19h<p/>
+              3º Domingo - Culto com a Família - 19h<p/>
+              4º Domingo - Domingo Profético - 19h
+            </h6>
+          </div>
+          <div className='terca'>
+            <strong>Terça</strong>
+            <h6>
+              Oração e Estudo da Palavra - 20h
+            </h6>
+          </div>
+          <div className='quinta'>
+            <strong>Quinta</strong>
+            <h6>
+              Culto da Vitória - 20h
+            </h6>
+          </div>
+          <div className='sabado'>
+            <strong>Sábado</strong>
+            <h6>
+              1º Sábado - (Sem Culto)<p/>
+              <p/><br/>
+
+              2º Sábado - Jovens & Orquestra - 20h<p/>
+              3º Sábado - Circulo de Oração - 20h<p/>
+              4º Sábado - Evangelismo - 20h<p/>
+              5º Sábado - Departamento Infantil - 20h
+            </h6>
+          </div>
+        </div>
+        <div className='cultos_vilaSandra'>
+        <span>Vila Sandra</span>
+          <div className='domingo'>
+            <strong>Domingo</strong>
+            <h6>
+              
+            </h6>
+          </div>
+          <div className='quarta'>
+            <strong>Terça</strong>
+            <h6>
+              
+            </h6>
+          </div>
+          <div className='sabado'>
+            <strong>Sábado</strong>
+            <h6>
+              
+            </h6>
+          </div>
+        </div>
+      </div>
     </div>
     </>
   )
