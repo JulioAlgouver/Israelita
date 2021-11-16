9c16e837754353045c629677ab0336658f87c4a2
JulioAlgouver <julioalgouver@gmail.com>
2021-11-16 17:53:40 -0300

    Initialize project using Create React App

--- src/pages/Galeria/index.tsx
+++ src/pages/Galeria/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Menu from "../../components/menu/menu";
 
+//CSS
+import './index.css'; 
+
 const Galeria: React.FC = () => {
   return(
     <>
       <Menu/>
-      <div>
-        
+      <div id='main_label_galeria'>
+        <div className='content'>
+
+        </div>
       </div>
     </>
   )
