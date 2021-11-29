2a8e46857757541a6393bc9c9ac3ac530197eb85
JulioAlgouver <julioalgouver@gmail.com>
2021-11-29 11:56:16 -0300

    Initialize project using Create React App

--- src/pages/Galeria/Fotos/gallery.tsx
+++ src/pages/Galeria/Fotos/gallery.tsx
@@ -3,11 +3,20 @@ import React from "react";
 //CSS
 import './gallery.css';
 
+//IMAGES
+import foto1 from './IgrejaSede/sede.jpg';
+import foto2 from './IgrejaSede/sede_1.jpg';
+import foto3 from './IgrejaSede/sede_2.jpg';
+import foto4 from './IgrejaSede/sede_3.jpg';
+
 export const GaleriaSede: React.FC = () => {
   return(
       <div id='photo-gallery-container'>
         <div className='content'>
-
+          <img src={foto1}></img>
+          <img src={foto2}></img>
+          <img src={foto3}></img>
+          <img src={foto4}></img>
         </div>
       </div>    
   )
