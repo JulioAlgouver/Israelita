6d94b076ee0b752a0a8d6c09433ab9a25bec83f1
JulioAlgouver <julioalgouver@gmail.com>
2021-11-29 12:12:51 -0300

    Initialize project using Create React App

--- src/pages/Galeria/Fotos/gallery.tsx
+++ src/pages/Galeria/Fotos/gallery.tsx
@@ -4,19 +4,25 @@ import React from "react";
 import './gallery.css';
 
 //IMAGES
-import foto1 from './IgrejaSede/sede.jpg';
-import foto2 from './IgrejaSede/sede_1.jpg';
-import foto3 from './IgrejaSede/sede_2.jpg';
-import foto4 from './IgrejaSede/sede_3.jpg';
+import sede1 from './IgrejaSede/sede.jpg';
+import sede2 from './IgrejaSede/sede_1.jpg';
+import sede3 from './IgrejaSede/sede_2.jpg';
+import sede4 from './IgrejaSede/sede_3.jpg';
+
+import vilaSandra1 from './VilaSandra/vilaSandra.jpg';
+import vilaSandra2 from './VilaSandra/vilaSandra_1.jpg';
+import vilaSandra3 from './VilaSandra/vilaSandra_2.jpg';
+import vilaSandra4 from './VilaSandra/vilaSandra_3.jpg';
+import vilaSandra5 from './VilaSandra/vilaSandra_4.jpg';
 
 export const GaleriaSede: React.FC = () => {
   return(
       <div id='photo-gallery-container'>
         <div className='content'>
-          <img src={foto1}></img>
-          <img src={foto2}></img>
-          <img src={foto3}></img>
-          <img src={foto4}></img>
+          <img src={sede1}></img>
+          <img src={sede2}></img>
+          <img src={sede3}></img>
+          <img src={sede4}></img>
         </div>
       </div>    
   )
@@ -26,7 +32,11 @@ export const GaleriaVilaSandra: React.FC = () => {
   return(
       <div id='photo-gallery-container'>
         <div className='content'>
-
+          <img src={vilaSandra1}></img>
+          <img src={vilaSandra2}></img>
+          <img src={vilaSandra3}></img>
+          <img src={vilaSandra4}></img>
+          <img src={vilaSandra5}></img>
         </div>
       </div>   
   )
