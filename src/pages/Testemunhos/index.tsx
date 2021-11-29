61c520fa768abc6e6117cb5b4be542697487fa4e
JulioAlgouver <julioalgouver@gmail.com>
2021-11-29 12:59:51 -0300

    Initialize project using Create React App

--- src/pages/Testemunhos/index.tsx
+++ src/pages/Testemunhos/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Popup from "reactjs-popup";
 import Menu from "../../components/menu/menu";
 
 //CSS
 import './index.css';
+
+//IMAGENS
+import btn_newTest from '../../images/btn_novoTestemunho.png';
 
 const Testemunhos: React.FC = () => {
   return(
@@ -11,6 +15,15 @@ const Testemunhos: React.FC = () => {
       <div id='main_label_testemunhos'>
         <div className='content'>
             
+        </div>
+        <div className='novoTestemunho'>
+          <Popup trigger={
+            <a>
+              <span>Deixe seu testemunho!</span>
+              <img src={btn_newTest}/>
+            </a>
+          }>
+          </Popup>
         </div>
       </div>
     </>
