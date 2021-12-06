54c4846fd78eef04cf95b02e12a30dd38f60ea9f
JulioAlgouver <julioalgouver@gmail.com>
2021-12-06 17:12:42 -0300

    Initialize project using Create React App

--- src/pages/Galeria/Fotos/gallery.tsx
+++ src/pages/Galeria/Fotos/gallery.tsx
@@ -25,6 +25,30 @@ import aniversario7 from './7anos/Foto_7.jpeg';
 import aniversario8 from './7anos/Foto_8.jpeg';
 import aniversario9 from './7anos/Foto_9.jpeg';
 import aniversario10 from './7anos/Foto_10.jpeg';
+import aniversario11 from './7anos/Foto_11.jpg';
+import aniversario12 from './7anos/Foto_12.jpg';
+import aniversario13 from './7anos/Foto_13.jpg';
+import aniversario14 from './7anos/Foto_14.jpg';
+import aniversario15 from './7anos/Foto_15.jpg';
+import aniversario16 from './7anos/Foto_16.jpg';
+import aniversario17 from './7anos/Foto_17.jpg';
+import aniversario18 from './7anos/Foto_18.jpg';
+import aniversario19 from './7anos/Foto_19.jpg';
+import aniversario20 from './7anos/Foto_20.jpg';
+import aniversario21 from './7anos/Foto_21.jpg';
+import aniversario22 from './7anos/Foto_22.jpg';
+import aniversario23 from './7anos/Foto_23.jpg';
+import aniversario24 from './7anos/Foto_24.jpg';
+import aniversario25 from './7anos/Foto_25.jpg';
+import aniversario26 from './7anos/Foto_26.jpg';
+import aniversario27 from './7anos/Foto_27.jpg';
+import aniversario28 from './7anos/Foto_28.jpg';
+import aniversario29 from './7anos/Foto_29.jpg';
+import aniversario30 from './7anos/Foto_30.jpg';
+import aniversario31 from './7anos/Foto_31.jpg';
+import aniversario32 from './7anos/Foto_32.jpg';
+import aniversario33 from './7anos/Foto_33.jpg';
+import aniversario34 from './7anos/Foto_34.jpg';
 
 
 export const GaleriaSede: React.FC = () => {
@@ -68,6 +92,30 @@ export const Galeria7anos: React.FC = () => {
           <img src={aniversario8}></img>
           <img src={aniversario9}></img>
           <img src={aniversario10}></img>
+          <img src={aniversario11}></img>
+          <img src={aniversario12}></img>
+          <img src={aniversario13}></img>
+          <img src={aniversario14}></img>
+          <img src={aniversario15}></img>
+          <img src={aniversario16}></img>
+          <img src={aniversario17}></img>
+          <img src={aniversario18}></img>
+          <img src={aniversario19}></img>
+          <img src={aniversario20}></img>
+          <img src={aniversario21}></img>
+          <img src={aniversario22}></img>
+          <img src={aniversario23}></img>
+          <img src={aniversario24}></img>
+          <img src={aniversario25}></img>
+          <img src={aniversario26}></img>
+          <img src={aniversario27}></img>
+          <img src={aniversario28}></img>
+          <img src={aniversario29}></img>
+          <img src={aniversario30}></img>
+          <img src={aniversario31}></img>
+          <img src={aniversario32}></img>
+          <img src={aniversario33}></img>
+          <img src={aniversario34}></img>
         </div>
       </div>   
   )
