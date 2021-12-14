8a11ed6804a7dd284c2751893d719675f7d217a7
JulioAlgouver <julioalgouver@gmail.com>
2021-12-14 16:55:43 -0300

    Initialize project using Create React App

--- src/pages/Testemunhos/New/index.tsx
+++ src/pages/Testemunhos/New/index.tsx
@@ -7,7 +7,21 @@ const NovoTestemunho: React.FC = () => {
   return(
     <>
       <div id='novoTestemunhoContainer'>
-      
+        <form>
+          <label htmlFor="nome">Nome</label>
+          <input type='text' className='nome' name='nome' placeholder='Informe seu nome'></input>
+
+          <label htmlFor="email">Email</label>
+          <input type='email' className='email' name='email' placeholder='Seu email'></input>
+
+          <label htmlFor="telefone">Telefone</label>
+          <input type='tel' className='phone' name='phone' placeholder='Seu telefone'></input>
+
+          <label htmlFor="testemunho">Seu Testemunho</label>
+          <textarea className='testemunho' name='testemunho' placeholder='Nos conte seu testemunho'></textarea>
+
+          <input type="submit" value="Enviar" />
+        </form>
       </div>
     </>
   )
