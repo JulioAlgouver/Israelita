c31a64d764508f25a6c87f97712e3dd7edd519ae
JulioAlgouver <julioalgouver@gmail.com>
2021-12-14 14:49:37 -0300

    Initialize project using Create React App

--- src/pages/Testemunhos/New/index.tsx
+++ src/pages/Testemunhos/New/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+//CSS
+import './index.css';
+
 const NovoTestemunho: React.FC = () => {
   return(
     <>
+      <div id='novoTestemunhoContainer'>
 
+      </div>
     </>
   )
 }
