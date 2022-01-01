5fae919db68ecb9432774f46c38880380a30d485
JulioAlgouver <julioalgouver@gmail.com>
2022-01-01 13:51:29 -0300

    Initialize project using Create React App

--- src/pages/Testemunhos/New/index.tsx
+++ src/pages/Testemunhos/New/index.tsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, {useState} from 'react';
 
 //CSS
 import './index.css';
 
 const NovoTestemunho: React.FC = () => {
+
+  const [campos, setCampos] = useState({
+      nome: '',
+      email: '',
+      phone: '',
+      testemunho: ''
+  });
+
+  function handleInputChange(event:any){
+    if(event.target.name === "anexo")
+      campos[event.target.name] = event.target.files[0];
+    else
+      campos[event.target.name] = event.target.value;
+    setCampos(campos);
+}
+
+  function handleFormSubmit(event:any){
+    event.preventDefault();
+    console.log(campos);
+  }
+
   return(
     <>
       <div id='novoTestemunhoContainer'>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <label htmlFor="nome">Nome</label>
-          <input type='text' className='nome' name='nome' placeholder='Informe seu nome'></input>
+          <input type='text' className='nome' name='nome' placeholder='Informe seu nome' onChange={handleInputChange}></input>
 
           <label htmlFor="email">Email</label>
-          <input type='email' className='email' name='email' placeholder='Seu email'></input>
+          <input type='email' className='email' name='email' placeholder='Seu email' onChange={handleInputChange}></input>
 
           <label htmlFor="telefone">Telefone</label>
-          <input type='tel' className='phone' name='phone' placeholder='Seu telefone'></input>
+          <input type='tel' className='phone' name='phone' placeholder='Seu telefone' onChange={handleInputChange}></input>
 
           <label htmlFor="testemunho">Seu Testemunho</label>
-          <textarea className='testemunho' name='testemunho' placeholder='Nos conte seu testemunho'></textarea>
+          <textarea className='testemunho' name='testemunho' placeholder='Nos conte seu testemunho' onChange={handleInputChange}></textarea>
 
-          <input type="submit" value="Enviar" />
+          <input type="submit" value="Enviar" className='btn_enviar'/>
         </form>
       </div>
     </>
