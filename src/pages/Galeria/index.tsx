ec4323ede010fbd3cba04d875bf060301072bb98
JulioAlgouver <julioalgouver@gmail.com>
2022-01-05 10:23:52 -0300

    Initialize project using Create React App

--- src/pages/Galeria/index.tsx
+++ src/pages/Galeria/index.tsx
@@ -9,6 +9,11 @@ import './index.css';
 import sede from './Fotos/IgrejaSede/sede.jpg';
 import vilaSandra from './Fotos/VilaSandra/vilaSandra.jpg';
 import capaAniversario from './Fotos/7anos/capa.jpg';
+import capaConfraternizacaoVitoriaEmCristo2021 from './Fotos/ConfraternizaçãoVitoriaEmCristo2021/ContrafernizacaoVitoriaEmCristo_1.jpg';
+import capaCultoNatalino2021 from './Fotos/CultoNatalino2021/Capa.jpg';
+import capaCultoAnoNovo2021 from './Fotos/CultodaVirada2021/Capa.jpg';
+
+
 //COMPONENTS
 import {Galeria7anos, GaleriaSede, GaleriaVilaSandra} from "./Fotos/gallery";
 
@@ -29,6 +34,18 @@ const Galeria: React.FC = () => {
               </Popup>
 
               <Popup trigger={<a><img src={capaAniversario}/><figcaption>7º Aniversário IEADMI</figcaption></a>}>
+                <Galeria7anos/>
+              </Popup>
+
+              <Popup trigger={<a><img src={capaConfraternizacaoVitoriaEmCristo2021}/><figcaption>Confraternização Vitória em Cristo 2021</figcaption></a>}>
+                <Galeria7anos/>
+              </Popup>
+
+              <Popup trigger={<a><img src={capaCultoNatalino2021}/><figcaption>Culto Especial de Natal 2021</figcaption></a>}>
+                <Galeria7anos/>
+              </Popup>
+
+              <Popup trigger={<a><img src={capaCultoAnoNovo2021}/><figcaption>Culto da Virada 2021/2022</figcaption></a>}>
                 <Galeria7anos/>
               </Popup>
             </div>
